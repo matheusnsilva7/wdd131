50db81a9615c61a1eeecc2404dda72595f8cd4f7
Matheus <silva.matheus6081@gmail.com>
2025-09-06 01:03:46 -0300

    first commit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,5 @@
-// Current Year
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 
-// Last Modified
 document.getElementById(
   "lastModified"
 ).textContent = `Last Modified: ${document.lastModified}`;
